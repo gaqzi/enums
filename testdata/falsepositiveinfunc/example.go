ed08935efb7eca9aa598dffdbf67022b3ab6ad7c
Björn Andersson <ba@sanitarium.se>
2025-03-27 22:48:43 +0100

    Ignore declarations not at the top level
    Or, at least, a best approximation based on levels away from the top
    the variable is declared. Not sure this will be a great solution for
    how to work it, but it's something at least. 😅
    
    Fixes #38.

---
+++ testdata/falsepositiveinfunc/example.go
@@ -0,0 +1,19 @@
+package falsepositiveinfunc
+
+type Flag string
+
+const (
+	DeployAllTheThings Flag = "deploy-all-the-things"
+)
+
+var AnotherExample Flag = "hello-there"
+
+// SlicedExample should be ignored, because I can't think of a case where this is what I'd like to match when it's declared top-level.
+var SlicedExample = []Flag{"hello", "there"}
+
+// FalsePositive is used to show a case where the flag is picked up as a declaration when it shouldn't be since it's not done at the file level.
+func FalsePositive() []Flag {
+	var myVar []Flag
+
+	return myVar
+}
